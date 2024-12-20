9118d86c4f79facf811a97b98c355dbb59b7039a
yeon <yeonnnnn.k@gmail.com>
2024-12-20 15:45:10 +0900

    🐛 Fix: input 공용 컴포넌트 props 및 타입 수정

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react';
+
 interface inputProps {
   placeholder: string;
   hasError?: boolean;
@@ -5,21 +7,25 @@ interface inputProps {
   type?: string;
 }
 
-// forwardRef<HTMLInputElement, InputProps>(({ hasError, errorMessage, placeholder }, ref)
-const Input = ({ placeholder, type, hasError, errorMessage }: inputProps) => {
-  return (
-    <div>
-      <input
-        // ref={ref}
-        placeholder={placeholder}
-        type={type}
-        className="w-full h-[40px] p-2.5 border focus:border-Green placeholder:text-LightGray rounded outline-none"
-      />
-      {hasError && (
-        <p className="text-Green text-description">{errorMessage}</p>
-      )}
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, inputProps>(
+  ({ placeholder, type, hasError, errorMessage, ...rest }, ref) => {
+    return (
+      <div>
+        <input
+          ref={ref}
+          placeholder={placeholder}
+          type={type}
+          className="w-full h-[40px] p-2.5 border focus:border-Green placeholder:text-LightGray rounded outline-none"
+          {...rest}
+        />
+        {hasError && errorMessage && (
+          <p className="text-Green text-description">{errorMessage}</p>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'input';
 
 export default Input;
