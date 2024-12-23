fff16ec6636a898eed2f4f31006200ecdc0dc01d
yeon <yeonnnnn.k@gmail.com>
2024-12-23 14:51:48 +0900

    ✨ Add: button-Loading Spinner 추가

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,5 +1,24 @@
+'use client';
+
+import Button from '@/components/Button/Button';
+
 const SignUp = () => {
-  return;
+  const test = () => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 2000); // 2초 후 완료
+    });
+  };
+
+  return (
+    <Button
+      onClick={async () => {
+        await test();
+        alert('loading spinner test');
+      }}
+    >
+      확인
+    </Button>
+  );
 };
 
 export default SignUp;
