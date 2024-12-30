ca6514cca06c1cf5a50a2f872abc3f3ffe48d5bc
yeon <yeonnnnn.k@gmail.com>
2024-12-30 17:18:40 +0900

    ✨ Add: kakao map 띄우기

--- src/app/map/component/MapListWrap.tsx
+++ src/app/map/component/MapListWrap.tsx
@@ -7,12 +7,11 @@ import chevron from '@icons/chevron_gray.svg';
 
 export const MapListWrap = () => {
   return (
-    <div className="bg-slate-200 fixed bottom-0 w-full h-32 bg-red-1 flex flex-col items-center pt-5 rounded-t-2xl shadow-mapListShadow">
+    <div className="bg-white fixed bottom-0 w-full h-32 bg-red-1 flex flex-col items-center pt-5 rounded-t-2xl shadow-mapListShadow z-zMapModal">
       <Image
         src={chevron}
         alt="화살표 아이콘"
         width={16}
-        height={10}
         quality={10}
         className="pb-4"
       />
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,10 +1,28 @@
 'use client';
+import { useEffect, useRef } from 'react';
 import { MapListWrap } from './component/MapListWrap';
 
 const Map = () => {
+  let latitude = 33.450701;
+  let longitude = 126.570667;
+
+  const mapRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    window.kakao.maps.load(() => {
+      const options = {
+        center: new window.kakao.maps.LatLng(latitude, longitude),
+        level: 3,
+      };
+
+      const map = new window.kakao.maps.Map(mapRef.current, options);
+    });
+  }, []);
+
   return (
-    <div className="relative w-full">
-      <MapListWrap />
+    <div className="relative w-full h-full">
+      <div ref={mapRef} className="w-full h-full">
+        <MapListWrap />
+      </div>
     </div>
   );
 };
