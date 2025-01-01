027997946d5624c1e1f240542049df0f1e078399
ara jo <ara.jo.song@gmail.com>
2025-01-01 17:08:24 +0900

    🚑️ Fix: change kakao map http to https api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Script
-          src={`http://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`}
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`}
           onLoad={() => {
             setMapScriptLoaded(true);
           }}
