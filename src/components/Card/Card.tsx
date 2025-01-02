82ae777055c19c6e8eb8f3e21d477317c62b4a9c
yeon <yeonnnnn.k@gmail.com>
2025-01-02 22:39:54 +0900

    ✨ Add: 캠핑장 상세페이지로 이동 추가

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import myWishIcon from '@icons/liked.svg';
 import notMyWishIcon from '@icons/not-liked.svg';
 import noImg from '@images/noImg.png';
+import Link from 'next/link';
 
 interface CardProps {
   itemId?: string;
@@ -21,34 +22,36 @@ const Card = ({
   description,
 }: CardProps) => {
   return (
-    <div className="w-10.5/12 rounded overflow-hidden">
-      <div className="relative">
-        <Image
-          src={imgSrc ? imgSrc : noImg}
-          alt="캠핑장 이미지"
-          width="100"
-          height="67"
-          layout="responsive"
-          className="rounded"
-          quality={30}
-        />
-        <Image
-          src={liked ? myWishIcon : notMyWishIcon}
-          alt="위시리스트"
-          width={20}
-          height={19}
-          className="absolute top-3 right-3"
-        />
-      </div>
+    <Link href={`/list/${itemId}`} className="w-full flex justify-center">
+      <div className="w-10.5/12 rounded overflow-hidden">
+        <div className="relative">
+          <Image
+            src={imgSrc ? imgSrc : noImg}
+            alt="캠핑장 이미지"
+            width="100"
+            height="67"
+            layout="responsive"
+            className="rounded"
+            quality={30}
+          />
+          <Image
+            src={liked ? myWishIcon : notMyWishIcon}
+            alt="위시리스트"
+            width={20}
+            height={19}
+            className="absolute top-3 right-3"
+          />
+        </div>
 
-      <div className="p-2">
-        <p className="text-content pb-1">{name}</p>
-        <p className="text-description">{address}</p>
-        <p className="text-DarkGray text-description">
-          {description ? description : '캠핑장의 정보를 업데이트 중입니다'}
-        </p>
+        <div className="p-2">
+          <p className="text-content pb-1">{name}</p>
+          <p className="text-description">{address}</p>
+          <p className="text-DarkGray text-description">
+            {description ? description : '캠핑장의 정보를 업데이트 중입니다'}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
