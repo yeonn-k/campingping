249b5f3cb16717933cfcbeb9ee7af92c95c348a4
yeon <yeonnnnn.k@gmail.com>
2025-01-29 22:02:28 +0900

    🐛 Fix: mixed content issue( http/https)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body>
         <Script
           strategy="beforeInteractive"
-          src={`http://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`}
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`}
         />
         <ClientLayout>{children}</ClientLayout>
       </body>
