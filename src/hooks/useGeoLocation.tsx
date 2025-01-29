dd9f6fceb05b464117b06150975591682ae9a3f1
yeon <yeonnnnn.k@gmail.com>
2025-01-29 19:58:55 +0900

    ♻️ Refactor: mobile에서 geoLocation query 없는 부분 해결

--- src/hooks/useGeoLocation.tsx
+++ src/hooks/useGeoLocation.tsx
@@ -1,12 +1,36 @@
 import { useEffect, useState } from 'react';
 
 const useGeoLocationPermission = () => {
-  const [isGeoLocationGranted, setIsGeoLocationGranted] = useState(true);
+  const [isGeoLocationGranted, setIsGeoLocationGranted] = useState<
+    boolean | null
+  >(null);
 
   useEffect(() => {
-    navigator.permissions.query({ name: 'geolocation' }).then((result) => {
-      setIsGeoLocationGranted(result.state === 'granted');
-    });
+    const checkLocationPermission = () => {
+      const localPermission = localStorage.getItem('geoPermission');
+      if (localPermission) {
+        setIsGeoLocationGranted(JSON.parse(localPermission));
+        return;
+      }
+
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          () => {
+            setIsGeoLocationGranted(true);
+            localStorage.setItem('geoPermission', 'true');
+          },
+          () => {
+            setIsGeoLocationGranted(false);
+            localStorage.setItem('geoPermission', 'false');
+          }
+        );
+      } else {
+        setIsGeoLocationGranted(false);
+        localStorage.setItem('geoPermission', 'false');
+      }
+    };
+
+    checkLocationPermission();
   }, []);
 
   return isGeoLocationGranted;
