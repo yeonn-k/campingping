ca65a0485ef50498b9fd9f1ede79b052fadb1528
yeon <yeonnnnn.k@gmail.com>
2025-01-31 19:37:19 +0900

    🐛 Modify: token error 백엔드 수정사항 반영

--- src/stores/userState.ts
+++ src/stores/userState.ts
@@ -16,6 +16,7 @@ export const userStore = create<UserState>()(
         if (v === null) {
           set({ userState: false, userEmail: null });
           localStorage.removeItem('user-storage');
+          localStorage.removeItem('token');
         } else {
           set({ userState: true, userEmail: v });
         }
