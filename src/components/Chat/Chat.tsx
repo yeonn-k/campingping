bfe144484fa294742b5c6a7fac0e62e5022e097d
yeon <yeonnnnn.k@gmail.com>
2025-02-14 18:29:33 +0900

    🔥

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -13,7 +13,6 @@ import goToBack from '@icons/goToBack.svg';
 import { ChatRooms } from '@/types/Chatting';
 import { chattingStore } from '@/stores/chattingState';
 
-import { timeFormat } from '@/utils/timeFormat';
 const Chat = () => {
   const [, setIsConnected] = useState(false);
   const [, setTransport] = useState('N/A');
